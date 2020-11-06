24b2411ed4a04a225ee0d4606dde40292430d4db
Emir Mertoglu <emirxmertoglu@outlook.com>
2020-11-07 00:54:23 +0300

    added kommunity and meetup

--- src/components/common/CommunitySocialInfo.vue
+++ src/components/common/CommunitySocialInfo.vue
@@ -1,7 +1,10 @@
 <template>
-  <p class="google-font mt-4 mb-3" style="font-size:130%">
+  <p class="google-font mt-4 mb-3" style="font-size: 130%">
     Follow Us:
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.facebook,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.facebook, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
@@ -18,7 +21,50 @@
       <span>Facebook</span>
     </v-tooltip>
 
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.twitter,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.kommunity, 0)"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          icon
+          v-on="on"
+          rel="noreferrer"
+          :href="config.generalConfig.socialLinks.kommunity"
+          target="_blank"
+          slot="activator"
+        >
+          <v-icon>mdi-temperature-kelvin</v-icon>
+        </v-btn>
+      </template>
+      <span>Kommunity</span>
+    </v-tooltip>
+
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.meetup, 0)"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          text
+          icon
+          v-on="on"
+          rel="noreferrer"
+          :href="config.generalConfig.socialLinks.meetup"
+          target="_blank"
+          slot="activator"
+        >
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-btn>
+      </template>
+      <span>Meetup</span>
+    </v-tooltip>
+
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.twitter, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
@@ -35,7 +81,10 @@
       <span>Twitter</span>
     </v-tooltip>
 
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.instagram,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.instagram, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
@@ -52,7 +101,10 @@
       <span>Instagram</span>
     </v-tooltip>
 
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.github,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.github, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
@@ -68,7 +120,10 @@
       </template>
       <span>Github</span>
     </v-tooltip>
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.linkedin,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.linkedin, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
@@ -84,7 +139,10 @@
       </template>
       <span>Linkedin</span>
     </v-tooltip>
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.youtube,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.youtube, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
@@ -100,7 +158,10 @@
       </template>
       <span>YouTube</span>
     </v-tooltip>
-    <v-tooltip top v-if="checkExistance(config.generalConfig.socialLinks.medium,0)">
+    <v-tooltip
+      top
+      v-if="checkExistance(config.generalConfig.socialLinks.medium, 0)"
+    >
       <template v-slot:activator="{ on }">
         <v-btn
           text
